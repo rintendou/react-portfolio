f03ceac1366908755cbd82a0d78b7488fd694401
Danny Chen <dannychen8102@gmail.com>
2022-10-16 17:47:26 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/layout/layout';
+import Home from './components/home/home'
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/home/home.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import './home.scss';
+
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text">
+                <h1>
+                    Hi, <br /> I'm Danny
+                </h1>
+                <h2> 
+                    Undergraduate Student at the <a href='https://www.nyit.edu/'><em>New York Institute of Technology</em></a> & an Aspiring Fullstack Developer.
+                </h2>
+                <Link to='/contact' className="button"> CONTACT ME </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
